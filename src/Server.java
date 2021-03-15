4426224d47b7823b4cfddfc606eb61f7f53f2e04
EternalEffy <effyjeev@gmail.com>
2021-03-15 21:56:21 +0300

    внесла изменения в соотвествии с требованиями

--- src/Server.java
+++ src/Server.java
@@ -1,26 +1,40 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Server {
     private static Socket clientSocket;
     private static ServerSocket server;
+    private static BufferedReader in;
+    private static BufferedWriter out;
 
     public static void main(String[] args) {
-        try {
             try {
-                server = new ServerSocket(3310);
+                server = new ServerSocket(3310,1);
                 System.out.println("Сервер запущен");
                 while (true) {
                     clientSocket = server.accept();
-                    System.out.println(clientSocket.isConnected());
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                    if(clientSocket.isConnected()){
+                        System.out.println("Клиент " + new String(in.readLine().getBytes(StandardCharsets.UTF_8)) + " с ip адреса: " + clientSocket.getInetAddress() + " подключен");
+                        out.write("Подключение установлено" + "\n");
+                        out.flush();
+                    }
                 }
-            } finally {
-                System.out.println("Сервер закрыт");
-                server.close();
             }
-        }    catch (IOException e) {
+            catch (IOException e) {
                 e.printStackTrace();
             }
+            finally {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
     }
 }
